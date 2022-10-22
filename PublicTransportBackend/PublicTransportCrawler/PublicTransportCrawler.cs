5faf0a86eecc604d33f383bd727f4566ca9bd56a
Szymon Smykała <ctk7000@gmail.com>
2022-10-22 18:45:35 +0200

    cleaned up

--- PublicTransportBackend/PublicTransportCrawler/PublicTransportCrawler.cs
+++ PublicTransportBackend/PublicTransportCrawler/PublicTransportCrawler.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace PublicTransportBackend
+namespace PublicTransportCrawler
 {
     public static class PublicTransportCrawler
     {
