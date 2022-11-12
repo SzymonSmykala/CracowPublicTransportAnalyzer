f56ec9023750230c3b92fe7b3488b42150488f41
Szymon Smykała <ctk7000@gmail.com>
2022-11-12 17:56:47 +0100

    added unit tests

---
+++ PublicTransportBackend/PublicTransportCrawler.UnitTests/Vehicles/VehicleRequestFactoryTests.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using PublicTransportCrawler.Vehicles;
+
+namespace PublicTransportCrawler.UnitTests.Vehicles;
+
+[TestFixture]
+public class VehicleRequestFactoryTests
+{
+    private IVehicleRequestFactory _uut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _uut = new VehicleRequestFactory();
+    }
+    
+    [Test]
+    public void ShouldCreateGetTramsRequest()
+    {
+        // Arrange
+        
+        // Act
+        var actual = _uut.CreateGetTramRequest();
+        // Assert
+        actual.Method.Should().Be(HttpMethod.Get);
+        actual.RequestUri.Should().Be(Constants.TramsEndpoint);
+    }
+}
