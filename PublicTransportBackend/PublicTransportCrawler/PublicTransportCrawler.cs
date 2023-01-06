7e5c7aa4c1a9116d2e0f51057ea1aa1cc86deea0
Szymon Smykała <ctk7000@gmail.com>
2023-01-06 15:51:46 +0100

    cleaned up

--- PublicTransportBackend/PublicTransportCrawler/PublicTransportCrawler.cs
+++ PublicTransportBackend/PublicTransportCrawler/PublicTransportCrawler.cs
@@ -10,57 +10,38 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using PublicTransportCrawler.Stops;
 using PublicTransportCrawler.Stops.Adapters;
 using PublicTransportCrawler.Storage.Repositories;
 using PublicTransportCrawler.Vehicles;
-using PublicTransportCrawler.Vehicles.Adapters;
 
 namespace PublicTransportCrawler
 {
     [ExcludeFromCodeCoverage]
     public class PublicTransportCrawler
     {
-        private readonly IVehicleService _vehicleService;
         private readonly IStopService _stopService;
         private readonly IDelayCalculator _delayCalculator;
         private readonly HttpClient _client;
         private readonly IDelayDataRepository _delayDataRepository;
         private readonly MyServerOptions _options;
-        private readonly ICurrentVehicleStateFacade _currentVehicleStateFacade;
-        private ILineCrawlerExecutor _lineCrawlerExecutor;
+        private readonly ILineCrawlerExecutor _lineCrawlerExecutor;
 
         public PublicTransportCrawler(IHttpClientFactory httpClientFactory,
-            IVehicleService service,
             IDelayDataRepository delayDataRepository,
             IOptions<MyServerOptions> options,
             IStopService stopService,
             IDelayCalculator delayCalculator,
-            ICurrentVehicleStateFacade currentVehicleStateFacade,
             ILineCrawlerExecutor lineCrawlerExecutor)
         {
-            this._client = httpClientFactory.CreateClient();
-            this._vehicleService = service;
+            _client = httpClientFactory.CreateClient();
             _delayDataRepository = delayDataRepository;
             _stopService = stopService;
             _delayCalculator = delayCalculator;
-            _currentVehicleStateFacade = currentVehicleStateFacade;
             _lineCrawlerExecutor = lineCrawlerExecutor;
             _options = options.Value;
         }
 
-        // [FunctionName("GetListOfVehicles")]
-        // public async Task<IActionResult> Run(
-        //     [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        //     ILogger log)
-        // {
-        //     var result = await _vehicleService.GetAllVehicles();
-        //     await _delayDataRepository.InsertSampleDataAsync();
-        //     Console.WriteLine(_options);
-        //     return new OkObjectResult(result);
-        // }
-        //
         [FunctionName("GetStopData")]
         public async Task<IActionResult> GetStopData(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -87,47 +68,6 @@ namespace PublicTransportCrawler
             });
             return new OkObjectResult(difference);
         }
-        
-        // [FunctionName("DelayCrawler")]
-        // public async Task Run([TimerTrigger("0 */10 * * * *")]TimerInfo myTimer, ILogger log)
-        // {
-        //     var result = await _stopService.GetRondoGrunwaldzkieDataAsync();
-        //     
-        //     var difference = result.Where(x => !string.IsNullOrEmpty(x.ActualTime)).ToList();
-        //     
-        //     difference.ForEach( x =>
-        //     {
-        //         try
-        //         {
-        //             var diff = _delayCalculator.Execute(x);
-        //             Console.WriteLine($"Delay in minutes: {diff.TotalMinutes} | Line: {x.PatternText} | Direction: {x.Direction} ");
-        //             _delayDataRepository.AddOrUpdateDelayData(x.TripId, "3338", diff, x.PatternText, x.Direction).GetAwaiter().GetResult();
-        //         }
-        //         catch(Exception e)
-        //         {
-        //             Console.WriteLine($"Message: {e.Message}");
-        //             Console.WriteLine($"Actual: {x.ActualTime} | Planned: {x.PlannedTime}");
-        //         }
-        //     
-        //     });
-        // }
-        
-        // [FunctionName("DelayCrawlerV2")]
-        // public async Task<IActionResult> Run(
-        //     [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        //     ILogger log)
-        // {
-        //     var tasks = new List<Task>
-        //     {
-        //         _lineCrawlerExecutor.ExecuteAsync("173"),
-        //         _lineCrawlerExecutor.ExecuteAsync("194"),
-        //         _lineCrawlerExecutor.ExecuteAsync("307"),
-        //         _lineCrawlerExecutor.ExecuteAsync("182"),
-        //     };
-        //
-        //     await Task.WhenAll(tasks);
-        //     return new OkObjectResult("FAKE OK ");
-        // }
 
         [FunctionName("DelayCrawlerV2TimeTriggered")]
         public async Task RunTriggerAsync([TimerTrigger("*/5 4-22 * * *")] TimerInfo myTimer, ILogger log)
